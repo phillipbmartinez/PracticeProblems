774ce86e61264f908ac1bb73cd6a98655f26b388
phillipbmartinez <phillip.martinez210@gmail.com>
2026-04-10 10:54:25 -0500

    Added problem #4 GetQualifiedPlayers, tested, and passed

--- PracticeProblems/UdemyCodingExercises/FirstTenProblems.cs
+++ PracticeProblems/UdemyCodingExercises/FirstTenProblems.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,6 +72,43 @@ namespace UdemyCodingExercises
             //return input
             //    .GroupBy(c => c)
             //    .ToDictionary(group => group.Key, group => group.Count());
+        }
+
+
+        // #4: Filter and select qualified players from a game leaderboard
+        // Implement the GetQualifiedPlayers method that takes a dictionary of player names and their scores.
+        // Your goal is to build a list of players who qualify for the next round based on the following rules:
+        // A player must have a score of at least 50 to qualify.
+        // If a player has a score of 100 or more, the process should stop immediately—this may indicate suspected cheating or a system rule violation.However, any players who qualified before this point should still be included in the result.
+        // Return the list of names of players who qualified before any disqualifying score was encountered.
+        internal static List<string> GetQualifiedPlayers(Dictionary<string, int> playerScores)
+        {
+            List<string> qualifiedPlayers = new List<string>();
+
+            foreach (KeyValuePair<string, int> player in playerScores)
+            {
+                if (player.Value >= 100)
+                {
+                    break;
+                }
+                else if (player.Value >= 50)
+                {
+                    qualifiedPlayers.Add(player.Key);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            // LINQ solution
+            // return playerScores
+            //     .TakeWhile(score => score.Value < 100)
+            //     .Where(score => score.Value >= 50)
+            //     .Select(score => score.Key)
+            //     .ToList();
+
+            return qualifiedPlayers;
         }
     }
 }
