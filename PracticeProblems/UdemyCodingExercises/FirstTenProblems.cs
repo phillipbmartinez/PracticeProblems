dc0445afccb6d0988da89bcb4dcb2f9034736d61
phillipbmartinez <phillip.martinez210@gmail.com>
2026-04-10 08:55:35 -0500

    Created FirstTenProblems class and added first problem method called ContainsNegative

---
+++ PracticeProblems/UdemyCodingExercises/FirstTenProblems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCodingExercises
+{
+    internal class FirstTenProblems
+    {
+        // #1: Check if a collection contains any negative numbers (⭐)
+        // Exercise description: Implement the ContainsNegative method that checks whether a sequence of integers contains at least one negative number.
+        internal static bool ContainsNegative(IEnumerable<int> numbers)
+        {
+            bool result = false;
+
+            foreach (int i in numbers)
+            {
+                if (i < 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
