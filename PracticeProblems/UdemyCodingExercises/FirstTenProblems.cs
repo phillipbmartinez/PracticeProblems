9e92e3b929d9273e3a077b74d6af49c2996016c2
phillipbmartinez <phillip.martinez210@gmail.com>
2026-04-10 09:22:20 -0500

    Added problem #2 SplitFullName and tested

--- PracticeProblems/UdemyCodingExercises/FirstTenProblems.cs
+++ PracticeProblems/UdemyCodingExercises/FirstTenProblems.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -26,6 +27,18 @@ namespace UdemyCodingExercises
             return result;
 
             // other solution: return numbers.Any(number => number < 0);
+        }
+
+
+        // #2: Split a full name into first and last names (⭐)
+        // Implement the SplitFullName method that takes a string representing a person's full name and returns a tuple with two values: first name and last name.
+        // Assume the full name contains exactly one space, separating the first and last name.
+        internal static (string firstName, string lastname) SplitFullName(string fullName)
+        {
+            string firstName = fullName.Split(" ")[0];
+            string lastName = fullName.Split(" ")[1];
+
+            return (firstName, lastName);
         }
     }
 }
