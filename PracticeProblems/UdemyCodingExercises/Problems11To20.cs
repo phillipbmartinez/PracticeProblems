68416efbe5bcddc96d803a01b9a4ac0c27705dd4
phillipbmartinez <phillip.martinez210@gmail.com>
2026-04-13 10:44:38 -0500

    Added problem #12, tested successfully

--- PracticeProblems/UdemyCodingExercises/Problems11To20.cs
+++ PracticeProblems/UdemyCodingExercises/Problems11To20.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -13,6 +16,42 @@ namespace UdemyCodingExercises
 {
     internal class Problems11To20
     {
+        // #12: Sum a range of values from a variable-length input
+        // Implement the SumInRange method that accepts a start index, an end index, and a variable-length list of numbers(using the params keyword).
+        // The method should return the sum of all numbers from the start index up to(but not including) the end index.
+        // For example, for start index 1, end index 4, and numbers[5, 10, 15, 20, 25, 30, 35] the result shall be 45 (10+15+20).
+        // If the start or end indices are outside the bounds of the array, adjust them accordingly:
+        // Clamp start to 0 if it's negative
+        // Clamp end to the array’s length if it's too large (larger than array's length)
+        // If start >= end, return 0
+        public static int SumInRange(int start, int end, params int[] numbers)
+        {
+            int sum = 0;
+
+            if (start < 0)
+            {
+                start = 0;
+            }
+
+            if (end > numbers.Length)
+            {
+                end = numbers.Length;
+            }
+
+            if (start >= end)
+            {
+                return 0;
+            }
+
+            foreach (int num in numbers[start..end])
+            {
+                sum += num;
+            }
+
+            return sum;
+
+            // Using LINQ: return numbers[start..end].Sum();
+        }
     }
 
     // #11: Read file content safely
