26a2d51e6c59fc2b843252445d602e63d8f39007
phillipbmartinez <phillip.martinez210@gmail.com>
2026-05-12 16:23:52 -0500

    Added problem #41 GetTopNRecentMessages and tested successfully

--- PracticeProblems/UdemyCodingExercises/Problems41To50.cs
+++ PracticeProblems/UdemyCodingExercises/Problems41To50.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static UdemyCodingExercises.Problems11To20;
 
 namespace UdemyCodingExercises
 {
     internal class Problems41To50
     {
+        // Problem #41: Get top N recent messages by timestamp
+        // Implement the GetTopNRecentMessages method to take a collection of Message objects and an integer n.Assume all messages are ordered by Timestamp.
+        // This method should skip all messages with a Timestamp older than today, and return the next n messages.
+        public record Message(string Content, DateTime Timestamp);
+        public static IEnumerable<Message> GetTopNRecentMessages(IEnumerable<Message> messages, int n)
+        {
+            if (messages is null)
+            {
+                throw new ArgumentNullException(
+                    nameof(messages),
+                    "Messages cannot be null.");
+            }
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(n),
+                    "Number of messages to take must be non-negative.");
+            }
+
+            IEnumerable<Message> oldMessages = 
+                messages
+                .SkipWhile(message => message.Timestamp < DateTime.Today)
+                .Take(n);
+
+            return oldMessages;
+        }
     }
 }
