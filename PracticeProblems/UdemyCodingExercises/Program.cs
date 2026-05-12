e0268f4f893137368877d183bd4e014ea45ca850
phillipbmartinez <phillip.martinez210@gmail.com>
2026-05-12 15:43:25 -0500

    Moved Problems31To40 file to completed folder. Created new Problems41To50 file

--- PracticeProblems/UdemyCodingExercises/Program.cs
+++ PracticeProblems/UdemyCodingExercises/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace UdemyCodingExercises
+﻿using static UdemyCodingExercises.Problems31To40;
+
+namespace UdemyCodingExercises
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var messages = new List<Message>
+            {
+                new Message("Hello", new DateTime(2025, 12, 1, 14, 30, 0)),
+                new Message("Hi", new DateTime(2025, 12, 1, 15, 0, 0)),
+                new Message("Hey", new DateTime(2025, 12, 1, 14, 0, 0))
+            };
 
+            IEnumerable<string> sortedMessages = Problems31To40.GetRecentMessages(messages);
         }
     }
 }
